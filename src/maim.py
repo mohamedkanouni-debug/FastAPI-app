c62dde1065ffdd9423866fb9a6e980b3b36cf180
mohamed <mohamedkanouni776@gmail.com>
2026-02-05 22:59:12 +0100

    skeleton is ready

--- src/maim.py
+++
@@ -1,4 +0,0 @@
-# to start virtual enveronment: py -m venv venv
-# to activate virtual environment: source venv/bin/activate
-# to install dependencies: pip install -r requirements.txt
-# in requiremnts.txt we have:
