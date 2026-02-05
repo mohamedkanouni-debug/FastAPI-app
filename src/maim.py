81df1ab6de6e99bb4cc752bc23dffed53d7d2b5c
mohamed <mohamedkanouni776@gmail.com>
2026-02-05 21:59:18 +0100

    getting started

---
+++ src/maim.py
@@ -0,0 +1,4 @@
+# to start virtual enveronment: py -m venv venv
+# to activate virtual environment: source venv/bin/activate
+# to install dependencies: pip install -r requirements.txt
+# in requiremnts.txt we have:
